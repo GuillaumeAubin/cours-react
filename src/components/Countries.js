e4b63030cf9b9b6eac0d42a7cab9eb43c9f9d367
Guillaume Aubin <aubinguillaume4@gmail.com>
2022-03-23 10:50:09 +0100

    countries .js

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import Card from "./Card";
 
 const Countries = () => {
   const [data, setData] = useState([]);
+  const [rangeValue, setRangeValue] = useState(36);
+
   // Le useEffect se joue lorsque le composant est monté
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
       .then((res) => setData(res.data));
   }, []);
+
   return (
     <div className="countries">
-      <h1>COUNTRIES</h1>
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+      </ul>
       <ul>
-        {data.map((country, index) => (
+        {data.slice(0, rangeValue).map((country, index) => (
           <Card key={index} country={country} />
         ))}
       </ul>
