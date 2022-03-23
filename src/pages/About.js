c8b1c4e7a86cefe1f8c6a10401fdb3b2c9557e46
Guillaume Aubin <aubinguillaume4@gmail.com>
2022-03-23 09:55:25 +0100

    First Commit

---
+++ src/pages/About.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+        consequatur sapiente officiis qui voluptatibus. Autem ea, nesciunt
+        doloribus alias modi magnam consequatur vel corporis nihil minus quasi
+        natus veniam. Assumenda dolor earum est magnam quis nostrum odit.
+        Inventore soluta, ad vitae ipsum animi perferendis unde repudiandae
+        laboriosam, veritatis et sed, pariatur nobis voluptatem facilis nam
+        eius! Sed autem praesentium aliquam beatae culpa officia esse repellat
+        libero! Expedita obcaecati quos, quaerat error sapiente voluptatem, hic
+        ipsam, vero consequuntur ea magni facere nulla. Labore libero magnam
+        nihil dignissimos. Odit fuga alias voluptate natus, amet, officiis a
+        nobis cumque recusandae repellendus doloribus voluptatum.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus nobis
+        neque, reprehenderit ab eius commodi fugit, molestias quis ratione
+        accusamus ad corporis. Iste cum dolores mollitia, eveniet repellat
+        labore magnam maiores laudantium, ad natus minus perferendis, eius
+        iusto. Nostrum aliquam quas ipsum omnis, rem repellat minus provident id
+        voluptates quam. Inventore quia similique adipisci quaerat ratione aut
+        alias eum possimus deleniti fugit dolorum accusamus asperiores id, cum
+        cupiditate ducimus aliquid? Perspiciatis soluta sapiente reprehenderit
+        iste sint. Culpa obcaecati dolores soluta eveniet odio voluptatem.
+        Optio, quia nostrum. Quisquam quae dolorum voluptate! Labore sed minima
+        ad aut beatae maxime facilis perspiciatis id iste quibusdam magni cumque
+        laudantium placeat sint fuga ducimus atque, ex accusamus. Accusamus
+        numquam excepturi eum iusto ducimus voluptas. Aperiam illo assumenda
+        sapiente pariatur dignissimos autem excepturi, laboriosam saepe quia
+        suscipit amet sint. Facilis, deleniti adipisci saepe et itaque modi,
+        nobis consequuntur sequi cupiditate delectus dolor autem eum soluta
+        nulla a. Laboriosam distinctio, veniam tempora inventore recusandae
+        maxime fugit asperiores? Ratione explicabo impedit soluta esse?
+        Molestiae provident quam neque quos ducimus quis reiciendis quae impedit
+        harum aperiam inventore, nihil aut dicta hic minus asperiores aliquam
+        beatae cum dolore. Enim corrupti minima sequi recusandae itaque culpa
+        laudantium amet similique, architecto dignissimos!
+      </p>
+    </div>
+  );
+};
+
+export default About;
